1e883848b1c399381b4ef3371d3673f8200916c7
Kiboko Gift <gkiboko@gmail.com>
2023-01-10 17:16:10 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import SideBar from "./components/sidebar";
+import Main from "./components/main";
+
+export default function App() {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  );
+}
---
+++ src/components/main.js
@@ -0,0 +1,9 @@
+import "./main.css";
+
+export default function Main() {
+  return (
+    <div className="main">
+      <h1>This is The main Page!</h1>
+    </div>
+  );
+}
